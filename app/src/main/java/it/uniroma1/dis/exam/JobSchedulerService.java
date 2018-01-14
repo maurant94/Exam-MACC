314297b77d9f19520800db0199dd03deb16da7ce
maurant94 <masto.antonio@hotmail.it>
2018-01-14 16:20:00 +0100

    Volley integration, and scheduler service

--- app/src/main/java/it/uniroma1/dis/exam/JobSchedulerService.java
+++ app/src/main/java/it/uniroma1/dis/exam/JobSchedulerService.java
@@ -9,6 +9,18 @@ import android.os.AsyncTask;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+
+import java.security.ProtectionDomain;
+
 public class JobSchedulerService extends JobService {
 
     @Override
@@ -32,26 +44,56 @@ public class JobSchedulerService extends JobService {
         public NotificationTask(JobParameters params) {
             this.params = params;
         }
+
         @Override
         protected Void doInBackground(Void... voids) {
             //TODO CALCOLARE I PRODOTTI IN SCADENZA E MANDARE NOTIFICA
-            NotificationCompat.Builder mBuilder =
-                    new NotificationCompat.Builder(getApplicationContext())
-                            .setSmallIcon(R.drawable.ic_shopping_cart)
-                            .setContentTitle("EXPIRING PRODUCTS")
-                            .setContentText("5 PRODUCTS ARE EXPIRING");
+            String url = getString(R.string.url_backend);
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+            // prepare the Request
+            JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONObject>()
+                    {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Log.e("Response", response.toString());
+                                Gson gson = new Gson();
+                                Products[] products = gson.fromJson(response.toString(), Products[].class);
+                                //NOW BUILD NOTIFICATION
+                                if (products != null && products.length > 0) {
+                                    NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(getApplicationContext(), "MY_CHANNEL_ID")
+                                        .setSmallIcon(R.drawable.ic_shopping_cart)
+                                        .setContentTitle("EXPIRING PRODUCTS")
+                                        .setContentText(products.length + " PRODUCTS ARE EXPIRING");
 
-            Intent resultIntent = new Intent(getApplicationContext(), LoginActivity.class);
-            PendingIntent resultPendingIntent = PendingIntent.getActivity(
-                    getApplicationContext(),
-                    0,
-                    resultIntent,
-                    PendingIntent.FLAG_UPDATE_CURRENT
+                                    Intent resultIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                                    PendingIntent resultPendingIntent = PendingIntent.getActivity(
+                                            getApplicationContext(),
+                                            0,
+                                            resultIntent,
+                                            PendingIntent.FLAG_UPDATE_CURRENT
+                                    );
+                                    mBuilder.setContentIntent(resultPendingIntent);
+                                    int mNotificationId = 001;
+                                    NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                                    mNotifyMgr.notify(mNotificationId, mBuilder.build());
+                                }
+                            }catch(Exception e){
+                                Log.e("Response", e.getMessage());
+                            }
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("Error.Response", error.toString());
+                        }
+                    }
             );
-            mBuilder.setContentIntent(resultPendingIntent);
-            int mNotificationId = 001;
-            NotificationManager mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            mNotifyMgr.notify(mNotificationId, mBuilder.build());
+            // add it to the RequestQueue
+            queue.add(getRequest);
 
             jobFinished( params, false );
 
