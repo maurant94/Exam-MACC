4b1960166a5c90de4f40174e300457fa6465f597
maurant94 <masto.antonio@hotmail.it>
2018-01-22 13:33:47 +0100

    modifiche temporanee per fare login con backend in locale

--- app/src/main/java/it/uniroma1/dis/exam/LoginActivity.java
+++ app/src/main/java/it/uniroma1/dis/exam/LoginActivity.java
@@ -7,6 +7,14 @@ import android.util.Log;
 import android.view.View;
 
 import it.uniroma1.dis.exam.R;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -14,6 +22,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A login screen that offers login via email/password.
@@ -31,12 +47,12 @@ public class LoginActivity extends AppCompatActivity {
         //configure
         //https://developers.google.com/identity/sign-in/android/start-integrating
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.server_Client_id))
                 .requestEmail()
                 .build();
         //check already signed user
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-
         SignInButton signInButton = findViewById(R.id.sign_in_button);
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,9 +80,45 @@ public class LoginActivity extends AppCompatActivity {
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.e("token",account.getIdToken());
+            //try log in auth/google_oauth2
+            String url = getString(R.string.url_backend) + "signin";
+            RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+            // prepare the Request
+            Gson gson = new Gson();
+            String jsonString = account.getIdToken();
+            JSONObject obj = null;
+            try {
+                obj = new JSONObject();
+                obj.put("idToken", jsonString);
+            } catch (JSONException e) {
+                Log.e("Response", e.getMessage());
+            }
+            JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.POST, url, obj,
+                    new Response.Listener<JSONObject>()
+                    {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                Log.e("Response", response.toString());
+                            }catch(Exception e){
+                                Log.e("Error", e.getMessage());
+                            }
+                        }
+                    },
+                    new Response.ErrorListener()
+                    {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("Error.Response", error.toString());
+                        }
+                    }
+            );
+            // add it to the RequestQueue
+            queue.add(getRequest);
 
-            Intent i = new Intent(this,MainActivity.class);
-            startActivity(i);
+            //Intent i = new Intent(this,MainActivity.class);
+            //startActivity(i);
 
             //TODO - intent to next page
         } catch (ApiException e) {
